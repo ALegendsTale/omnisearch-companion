069f8ad76814b0f51012dc29023292e696b08341
ALegendsTale <brandon@jollybandit.com>
2024-02-18 16:04:39 -0500

    Move omnisearch types

--- src/popup/popup.ts
+++ src/popup/popup.ts
@@ -1,29 +1,3 @@
-type OmnisearchApi = {
-    // Returns a promise that will contain the same results as the Vault modal
-    search: (query: string) => Promise<ResultNoteApi[]>,
-    // Refreshes the index
-    refreshIndex: () => Promise<void>
-    // Register a callback that will be called when the indexing is done
-    registerOnIndexed: (callback: () => void) => void,
-    // Unregister a callback that was previously registered
-    unregisterOnIndexed: (callback: () => void) => void,
-}
-  
-type ResultNoteApi = {
-    score: number
-    vault: string
-    path: string
-    basename: string
-    foundWords: string[]
-    matches: SearchMatchApi[]
-    excerpt: string
-}
-  
-type SearchMatchApi = {
-    match: string
-    offset: number
-}
-
 let myPort = browser.runtime.connect({ name: 'popup' });
 myPort.onMessage.addListener((res) => {
     let { query, notes } = res as {query: string, notes: ResultNoteApi[]};
