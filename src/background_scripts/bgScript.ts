922bf9d8ed9e7096b9c7d223e822bbfb617c4c82
ALegendsTale <brandon@jollybandit.com>
2024-02-27 14:21:33 -0500

    Reload on tab changes
    + reload on tab changes
    + fix query bugs
    + wait for loading getNotes fixed
    - Replace activeTab permission with tabs

--- src/background_scripts/bgScript.ts
+++ src/background_scripts/bgScript.ts
@@ -1,22 +1,50 @@
 import Storage from "../utils/storage";
 
 const storage = new Storage();
-
-let query: string;
-
+let query: string | null;
 let port: browser.runtime.Port;
-
 let notes: ResultNoteApi[];
+let loading = false;
+
+const waitForLoading = () => new Promise(resolve => {
+    const checkLoading = setInterval(() => {
+        if (!loading) {
+            clearInterval(checkLoading);
+            resolve(true);
+        }
+    }, 100);
+});
+
+/**
+ * Retrieve query when tab changes & reload notes
+ */
+browser.tabs.onActivated.addListener(async (tab) => {
+    loading = true;
+    browser.tabs.get(tab.tabId).then(async (tabDetails) => {
+        const params = new URLSearchParams(tabDetails.url);
+        const tabQuery = params.get('q');
+        console.info(`Received query: ${tabQuery} from tab change, loading suggestions`);
+        query = tabQuery;
+        notes = await getNotes();
+        loading = false;
+    })
+})
 
 /**
  * Receives query from cScript & loads notes
  */
-browser.runtime.onMessage.addListener(async (message: {value: any, sender?: string}) => {
+browser.runtime.onMessage.addListener(async (message: {value: any, sender?: string}, sender) => {
+    loading = true;
     if(message.sender === 'cScript'){
-        console.info(`Received query: ${message.value}, loading suggestions`);
-        query = message.value;
-        // Preload badge prior to popup initialization & load notes for query
-        notes = await getNotes();
+        let activeTab = await browser.tabs.query({ active: true, currentWindow: true });
+        // Make sure the content script sending the query is the active tab
+        if(activeTab[0].id === sender.tab?.id){
+            console.info(`Received query: ${message.value} from cScript, loading suggestions`);
+            query = message.value;
+            // Preload badge prior to popup initialization & load notes for query
+            notes = await getNotes();
+            loading = false;
+        }
     }
 })
 
@@ -34,6 +62,7 @@ browser.storage.onChanged.addListener(async (change) => {
 
 const portFn = async (popupPort: browser.runtime.Port) => {
     port = popupPort;
+    await waitForLoading();
     popupPort.postMessage({"query": query, "notes": notes});
 }
 
@@ -55,5 +84,7 @@ async function getNotes() {
 
         return notesFiltered;
     }
+    // Reset badge number
+    else browser.browserAction.setBadgeText({ text: '' });
     return [];
 }
