b299e892d729ed20dc822dc1a04f3274f61780f9
ALegendsTale <brandon@jollybandit.com>
2025-07-15 19:25:25 -0400

    Context menu, housekeeping
    + Added a context menu item #5. Users can now select the browser tab, text, or links and the item will appear. When clicked, the popup will open with the item loaded into OmnisearchCompanion

--- src/background_scripts/bgScript.ts
+++ src/background_scripts/bgScript.ts
@@ -1,156 +1,181 @@
 import browser from 'webextension-polyfill';
-import Storage from "../utils/storage";
-import { isManifestV3 } from '../utils/helpers';
+import Storage from '../utils/storage';
+import { splitURLSearchParams, browserAction, isCommonProtocol } from '../utils/helpers';
+
+type Tab = browser.Tabs.Tab;
 
 const storage = new Storage();
 let port: browser.Runtime.Port;
-let tabCached: browser.Tabs.Tab;
+/* Cached tab to prevent loading more than necessary */
+let tabCached: Tab;
 let loading = false;
 
 /**
  * Waits for loading variable to equal true
  */
-const waitForLoading = () => new Promise(resolve => {
-    const checkLoading = setInterval(() => {
-        if (!loading) {
-            clearInterval(checkLoading);
-            resolve(true);
-        }
-    }, 100);
+const waitForLoading = () =>
+	new Promise((resolve) => {
+		const checkLoading = setInterval(() => {
+			if (!loading) {
+				clearInterval(checkLoading);
+				resolve(true);
+			}
+		}, 100);
+	});
+
+/**
+ * Add context menu item
+ */
+browser.contextMenus.create({
+	id: 'omnisearchcompanion',
+	title: 'Omnisearch Companion',
+	contexts: ['selection', 'tab', 'link'],
 });
 
 /**
- * Removes search parameters from URL
+ * Get query from user interaction with context menu
  */
-function splitURLSearchParams(url: string | undefined) {
-    if(!url) return;
-    const urlObj = new URL(url);
-    const search = urlObj.search;
-    urlObj.search = '';
-    return {url: urlObj.toString(), search};
-}
+browser.contextMenus.onClicked.addListener(async (info, tab) => {
+	if (!tab) return;
+
+	if (info.selectionText) {
+		console.info(`Text selected, loading suggestions`);
+		browserAction.openPopup();
+		await loadNotes(info.selectionText.trim());
+	}
+	else if (info.linkUrl) {
+		console.info(`Link selected, loading suggestions`);
+		browserAction.openPopup();
+		await loadNotes(info.linkUrl);
+	}
+	else if (info.pageUrl) {
+		console.info(`Tab selected, loading suggestions`);
+		browserAction.openPopup();
+		await loadNotes(info.pageUrl);
+	}
+
+	tabCached = tab;
+});
 
 /**
  * Retrieve query when tab url changes & reload notes
  */
 browser.tabs.onUpdated.addListener(async (tabId, changedInfo, tab) => {
-    // Return early if no URL
-    if(!tab?.url) return;
-    // Always defined because of above check
-    const tabURL = splitURLSearchParams(tab.url)!;
-    const tabCachedURL = splitURLSearchParams(tabCached?.url);
-    // Ensure the URL was updated & the tab is active
-    if(changedInfo.url && tab.active){
-        // Only load on first update, or if tab URL / search changed
-        if(tabCached === undefined || tabURL.url !== tabCachedURL?.url || tabURL.search !== tabCachedURL?.search) {
-            tabCached = tab;
-            await loadNotes(tab);
-            console.info(`Tab updated, loading suggestions`);
-        }
-    }
-})
+	// Return early if no URL
+	if (!tab?.url) return;
+	// Always defined because of above check
+	const tabURL = splitURLSearchParams(tab.url)!;
+	const tabCachedURL = splitURLSearchParams(tabCached?.url);
+	// Ensure the URL was updated & the tab is active
+	if (changedInfo.url && tab.active) {
+		// Only load on first update, or if tab URL / search changed
+		if (tabCached === undefined || tabURL.url !== tabCachedURL?.url || tabURL.search !== tabCachedURL?.search) {
+			tabCached = tab;
+			await loadNotes(tab);
+			console.info(`Tab updated, loading suggestions`);
+		}
+	}
+});
 
 /**
  * Retrieve query when tab changes & reload notes
  */
 browser.tabs.onActivated.addListener(async (tab) => {
-    browser.tabs.get(tab.tabId).then(async (tab) => {
-        tabCached = tab;
-        await loadNotes(tab);
-        console.info(`Tab changed, loading suggestions`);
-    })
-})
+	browser.tabs.get(tab.tabId).then(async (tab) => {
+		tabCached = tab;
+		await loadNotes(tab);
+		console.info(`Tab changed, loading suggestions`);
+	});
+});
 
 /**
  * Reload after settings changes
  */
 browser.storage.onChanged.addListener(async (change, area) => {
-    // Run only if the storage type is sync
-    if(area === 'sync'){
-        // Load notes based on the last cached tab (avoids needing `activeTab` permission)
-        await loadNotes(tabCached);
-        console.info('Storage changed, reloading suggestions');
-        await sendMessage();
-    }
-})
+	// Run only if the storage type is sync
+	if (area === 'sync') {
+		// Load notes based on the last cached tab (avoids needing `activeTab` permission)
+		await loadNotes(tabCached);
+		console.info('Storage changed, reloading suggestions');
+		await sendMessage();
+	}
+});
 
 /**
  * Connects to `popup` each time it is opened. Creates initial notes on open & saves port for later use.
  */
 const portFn = async (popupPort: browser.Runtime.Port) => {
-    // Saves initial connection port for later use
-    port = popupPort;
-    await sendMessage();
-}
+	// Saves initial connection port for later use
+	port = popupPort;
+	await sendMessage();
+};
 
 // Create & connect port to `popup`
-if(browser.runtime.onConnect.hasListener(portFn)) browser.runtime.onConnect.removeListener(portFn);
+if (browser.runtime.onConnect.hasListener(portFn)) browser.runtime.onConnect.removeListener(portFn);
 browser.runtime.onConnect.addListener(portFn);
 
 /**
  * Sends a message to `popup.ts` to create notes
  */
 async function sendMessage() {
-    // Get query / notes from local storage
-    const { query, notes }: { query?: string, notes?: string } = await browser.storage.local.get([ "query", "notes" ]);
-    // Returns early if notes is null
-    if(!notes) return;
-    await waitForLoading();
-    // Only send message if query & port are non-null
-    if(port){
-        // Convert notes to object before sending
-        port.postMessage({"query": query, "notes": JSON.parse(notes)});
-    }
+	// Get query / notes from local storage
+	const { query, notes }: { query?: string; notes?: string } = await browser.storage.local.get(['query', 'notes']);
+	// Returns early if notes is null
+	if (!notes) return;
+	await waitForLoading();
+	// Only send message if query & port are non-null
+	if (port) {
+		// Convert notes to object before sending
+		port.postMessage({ query: query, notes: JSON.parse(notes) });
+	}
 }
 
 /**
  * Load notes after setting query/URL. Saves to local storage.
  * @param tab Browser tab
  */
-async function loadNotes (tab: browser.Tabs.Tab) {
-    loading = true;
-    // Return early if query isn't set
-    if(!tab?.url) return;
+async function loadNotes(_query: string | Tab) {
+	loading = true;
 
-    // Reset badge before loading
-    setBadge({ text: '' });
+	// Reset badge before loading
+	browserAction.setBadgeText({ text: '' });
 
-    const { searchType } = await storage.getSettingsStorage();
-    const url = new URL(tab.url);
-    let query: string | null = null;
-    // Set query based on searchType setting
-    switch(searchType) {
-        case 'Query':
-            query = url.searchParams.get('q');
-            break;
-        case 'URL':
-            query = isCommonProtocol(url.protocol) ? url.href : null;
-            break;
-        case 'Both':
-            // Set query as search paramter, otherwise set URL (which has an origin i.e. not browser native pages)
-            query = url.searchParams.get('q') || (isCommonProtocol(url.protocol) ? url.href : null);
-            break;
-    }
+	let query: string | null = null;
 
-    /**
-     * Checks if the protocol is commonly used by browsers
-     * @param protocol The protocol to check
-     * @returns boolean
-     */
-    function isCommonProtocol(protocol: string) {
-        let commonProtocols = ['http', 'https', 'file', 'ftp'];
-        return commonProtocols.some((commonProtocol) => protocol.includes(commonProtocol));
-    }
+	if(typeof _query === 'string') {
+		query = _query;
+	}
+	if(typeof _query === 'object') {
+		// Return early if query isn't set
+		query = await getURLQuery(_query) || null;
+	}
 
-    console.info(`Loading: ${query}`);
-    
-    // Save query to local storage
-    await browser.storage.local.set({ query });
-    // Get notes after query is set
-    let notes = await getNotes();
-    // Save string notes to local storage
-    await browser.storage.local.set({ notes: JSON.stringify(notes) });
-    loading = false;
+	async function getURLQuery(tabQuery: Tab) {
+		if (!tabQuery?.url) return;
+
+		const url = new URL(tabQuery.url);
+		const { searchType } = await storage.getSettingsStorage();
+		// Set query based on searchType setting
+		switch (searchType) {
+			case 'Query':
+				return url.searchParams.get('q');
+			case 'URL':
+				return isCommonProtocol(url.protocol) ? url.href : null;
+			case 'Both':
+				// Set query as search paramter, otherwise set URL (which has an origin i.e. not browser native pages)
+				return url.searchParams.get('q') || (isCommonProtocol(url.protocol) ? url.href : null);
+		}
+	}
+
+	console.info(`Loading: ${query}`);
+
+	// Save query to local storage
+	await browser.storage.local.set({ query });
+	// Get notes after query is set
+	let notes = await getNotes();
+	// Save string notes to local storage
+	await browser.storage.local.set({ notes: JSON.stringify(notes) });
+	loading = false;
 }
 
 /**
@@ -158,47 +183,35 @@ async function loadNotes (tab: browser.Tabs.Tab) {
  * @returns Notes array
  */
 async function getNotes() {
-    // Get query from local storage
-    const { query }: { query?: string } = await browser.storage.local.get([ "query" ]);
-    // Get settings from storage
-    const settings = await storage.getSettingsStorage();
-    // Skip fetch if query is null or undefined
-    if(query != null){
-        // If URL, encode before passing to Omnisearch
-        const response = await fetch(`http://localhost:${settings.port}/search?q=${URL.canParse(query) ? encodeURIComponent(query) : query}`).catch(() => null);
-        // If no response, exit
-        if(!response) {
-				// Set error badge
-				setBadge({ text: '🛇' });
-				console.error('Please ensure Obsidian is open, the Omnisearch HTTP server is enabled, and that the port in settings matches.');
-            return null;
-        }
-        // Get notes from fetch response
-        const notes: ResultNoteApi[] = await response.json()
-        // Filter notes by user defined score
-        const notesFiltered = notes.filter((note) => note.score > Number(settings.notesScore))
-        .sort((a, b) => b.score - a.score)
-        .slice(0, Number(settings.notesShown));
+	// Get query from local storage
+	const { query }: { query?: string } = await browser.storage.local.get(['query']);
+	// Get settings from storage
+	const settings = await storage.getSettingsStorage();
+	// Skip fetch if query is null or undefined
+	if (query != null) {
+		// If URL, encode before passing to Omnisearch
+		const response = await fetch(`http://localhost:${settings.port}/search?q=${URL.canParse(query) ? encodeURIComponent(query) : query}`).catch(() => null);
+		// If no response, exit
+		if (!response) {
+			// Set error badge
+			browserAction.setBadgeText({ text: '🛇' });
+			console.error('Please ensure Obsidian is open, the Omnisearch HTTP server is enabled, and that the port in settings matches.');
+			return null;
+		}
+		// Get notes from fetch response
+		const notes: ResultNoteApi[] = await response.json();
+		// Filter notes by user defined score
+		const notesFiltered = notes
+			.filter((note) => note.score > Number(settings.notesScore))
+			.sort((a, b) => b.score - a.score)
+			.slice(0, Number(settings.notesShown));
 
-        // Set badge to the number of notes fetched
-        setBadge({ text: notesFiltered.length !== 0 ? notesFiltered.length.toString() : '' });
+		// Set badge to the number of notes fetched
+		browserAction.setBadgeText({ text: notesFiltered.length !== 0 ? notesFiltered.length.toString() : '' });
 
-        return notesFiltered;
-    }
-    // Reset badge number
-    setBadge({ text: '' });
-    return [];
-}
-
-/**
- * Use `action` or `browserAction` depending on manifest
- */
-function setBadge(textDetails: browser.Action.SetBadgeTextDetailsType) {
-    // Set badge using function based on which manifest the extension is using
-    if(isManifestV3()){
-        browser.action.setBadgeText(textDetails);
-    }
-    else {
-        browser.browserAction.setBadgeText(textDetails);
-    }
+		return notesFiltered;
+	}
+	// Reset badge number
+	browserAction.setBadgeText({ text: '' });
+	return [];
 }
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -3,22 +3,48 @@ import browser from 'webextension-polyfill';
 /**
  * Checks if the extension is Manifest V3
  */
-export function isManifestV3(){
-    return browser.runtime.getManifest().manifest_version === 3;
+export function isManifestV3() {
+	return browser.runtime.getManifest().manifest_version === 3;
 }
+
+/**
+ * Use `action` or `browserAction` depending on manifest
+ */
+export const browserAction = isManifestV3() ? browser.action : browser.browserAction;
 
 /**
  * Returns a shortened string
  */
-export function getShortString(query: string, maxChars = 30){
-    // Do nothing if query doesn't need to be shortened
-    if(query.length < maxChars) return query;
+export function getShortString(query: string, maxChars = 30) {
+	// Do nothing if query doesn't need to be shortened
+	if (query.length < maxChars) return query;
 
-    const halfMax = maxChars / 2;
-    const halfLength = query.length / 2;
-    // Keep string length number of maxChars or less
-    const clampLength = halfLength <= halfMax ? halfLength : halfMax;
-    const firstChars = query.substring(0, clampLength);
-    const lastChars = query.substring(query.length - clampLength, query.length);
-    return `${firstChars}...${lastChars}`;
+	const halfMax = maxChars / 2;
+	const halfLength = query.length / 2;
+	// Keep string length number of maxChars or less
+	const clampLength = halfLength <= halfMax ? halfLength : halfMax;
+	const firstChars = query.substring(0, clampLength);
+	const lastChars = query.substring(query.length - clampLength, query.length);
+	return `${firstChars}...${lastChars}`;
+}
+
+/**
+ * Removes search parameters from URL
+ */
+export function splitURLSearchParams(url: string | undefined) {
+	if (!url) return;
+	const urlObj = new URL(url);
+	const search = urlObj.search;
+	urlObj.search = '';
+	return { url: urlObj.toString(), search };
+}
+
+/**
+ * Checks if the protocol is commonly used by browsers
+ * @param protocol The protocol to check
+ * @returns boolean
+ */
+export function isCommonProtocol(protocol: string) {
+	let commonProtocols = ['http', 'https', 'file', 'ftp'];
+	return commonProtocols.some((commonProtocol) => protocol.includes(commonProtocol));
 }
